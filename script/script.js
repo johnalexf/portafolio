7b1673cb79a65a56cc4a56a1a38eb0d4f284d518
johnalexf <johnalexfr@gmail.com>
2025-03-25 18:17:53 -0500

    haciendo responsive el navbar

--- script/script.js
+++ script/script.js
@@ -1,21 +1,43 @@
-let frasePrincipal = document.getElementById('frasePrincipal');
+let menuNavbar = document.querySelector('#menuNavbar');
+let desplegadoMenuNavbar = true;
 
-let texto = "Mi determinación para dar una vuelta de 150 km en bicicleta es igual a mi pasión por programar.";
-let indice = 0;
-let tamañoTexto = texto.length;
-
-
-function maquinaEscribir(){
-    frasePrincipal.innerHTML = ` <h2> ${texto.substring(0,indice++)}|</h2>`;
-    if(indice <= tamañoTexto){
-        setTimeout(maquinaEscribir, 50);
+function desplegarMenuNavbar(){
+    if(!desplegadoMenuNavbar){
+        desplegadoMenuNavbar = true;
+        menuNavbar.classList.remove('ocultar');
+        menuNavbar.classList.add('mostrar');
+        console.log("mostrar")
     }else{
-        frasePrincipal.innerHTML = ` <h2> ${texto.substring(0,indice++)}</h2>`; 
+        desplegadoMenuNavbar = false;
+        menuNavbar.classList.remove('mostrar');
+        menuNavbar.classList.add('ocultar');
+        console.log("ocultar")
+
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    maquinaEscribir();
-  });
+
+
+
+
+// let frasePrincipal = document.getElementById('frasePrincipal');
+
+// let texto = "Mi determinación para dar una vuelta de 150 km en bicicleta es igual a mi pasión por programar.";
+// let indice = 0;
+// let tamañoTexto = texto.length;
+
+
+// function maquinaEscribir(){
+//     frasePrincipal.innerHTML = ` <h2> ${texto.substring(0,indice++)}|</h2>`;
+//     if(indice <= tamañoTexto){
+//         setTimeout(maquinaEscribir, 50);
+//     }else{
+//         frasePrincipal.innerHTML = ` <h2> ${texto.substring(0,indice++)}</h2>`; 
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     maquinaEscribir();
+//   });
 
 
